6c648216996e79b610497b71795b0d0c61c310df
siriwatknp <siriwatkunaporn@gmail.com>
2024-08-23 14:28:33 +0700

    lesson 5 solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
 
 function App() {
   return (
-    <>
+    <Stack spacing={2}>
       <CssBaseline /> {/* Reset CSS */}
       <TextField
         label="Email"
@@ -30,7 +31,7 @@ function App() {
         variant="outlined"
       />
       <Button>Hello</Button>
-    </>
+    </Stack>
   );
 }
 
