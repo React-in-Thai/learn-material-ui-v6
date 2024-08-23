b28e26c0140b2aa9f09edde7c371a40340b629eb
siriwatknp <siriwatkunaporn@gmail.com>
2024-08-23 14:27:01 +0700

    lesson guide

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
+// 1. import the Stack component (https://mui.com/material-ui/react-stack/)
 
 function App() {
   return (
+    // 2. Wrap the TextField and Button components in a Stack component
+    //    adjust the spacing prop to `2` to add space between the components
     <>
       <CssBaseline /> {/* Reset CSS */}
       <TextField
