5b7f0c0a9ac21d7aae1d652220f364c30e4e9185
siriwatknp <siriwatkunaporn@gmail.com>
2024-08-23 14:18:17 +0700

    lesson guide

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
+// 1. import the TextField component (https://mui.com/material-ui/react-text-field/)
 
 function App() {
   return (
     <>
       <CssBaseline /> {/* Reset CSS */}
       <Button>Hello</Button>
+      {/* 2. render the email TextField with:
+        - an id of "email"
+        - a label of "Email"
+        - a placeholder of "Enter your email"
+        - a type of "email"
+        - other attributes that you think are necessary
+       */}
     </>
   );
 }
