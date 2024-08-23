df81c855577e920a4264aa3fb8f9aa08b872c953
siriwatknp <siriwatkunaporn@gmail.com>
2024-08-23 14:32:11 +0700

    lesson guide

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
+// 1. Import Box component from Material UI
 
 function App() {
   return (
+    // 2. Use Box component to wrap the Stack component
+    // 3. Apply padding to the `sx` prop of the Box component (https://mui.com/material-ui/customization/how-to-customize/#the-sx-prop)
     <Stack spacing={2}>
       <CssBaseline /> {/* Reset CSS */}
       <TextField
