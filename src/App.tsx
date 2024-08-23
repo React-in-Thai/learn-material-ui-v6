b6e492c71588dac478f9691ebcc484e02aadf81f
siriwatknp <siriwatkunaporn@gmail.com>
2024-08-23 14:21:29 +0700

    lesson 3 solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
 
 function App() {
   return (
     <>
       <CssBaseline /> {/* Reset CSS */}
+      <TextField
+        label="Email"
+        id="email"
+        type="email"
+        name="email"
+        placeholder="your@email.com"
+        autoComplete="email"
+        autoFocus
+        required
+        fullWidth
+        variant="outlined"
+      />
       <Button>Hello</Button>
     </>
   );
