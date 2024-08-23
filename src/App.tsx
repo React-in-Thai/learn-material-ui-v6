94ca9d7d1fe825b3da834dcf73ef58e272ae5a34
siriwatknp <siriwatkunaporn@gmail.com>
2024-08-23 13:53:21 +0700

    lesson guide

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
+// 1. Install Material UI (https://next.mui.com/material-ui/getting-started/installation/)
+
+// 2. Import the Button component from Material UI (https://next.mui.com/material-ui/react-button/)
+
 function App() {
+  // 3. Render the button (https://next.mui.com/material-ui/react-button/#basic-button)
   return <>Hello</>;
 }
 
