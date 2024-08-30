67b79e9436ee4b0c6c3b3560a42501ba9d2b9620
siriwatknp <siriwatkunaporn@gmail.com>
2024-08-30 13:47:44 +0700

    lesson guide

--- src/AppTheme.tsx
+++ src/AppTheme.tsx
@@ -17,6 +17,12 @@ const theme = createTheme({
   shape: {
     borderRadius: 8,
   },
+  // Use this template (https://mui.com/material-ui/getting-started/templates/sign-in/) as an expected result.
+  // 1. Target `components.MuiButton.styleOverrides.root` to theme the Button component
+  //    (https://mui.com/material-ui/customization/theme-components/#theme-style-overrides)
+  //
+  // 2. Customize only the combination of `contained` variant + `primary` color
+  //    (https://mui.com/material-ui/customization/theme-components/#variants)
 });
 
 export default function AppTheme({ children }: React.PropsWithChildren) {
