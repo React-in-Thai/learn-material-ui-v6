b592921b2760bafbe63d4ac6331ba70bd174bf6a
siriwatknp <siriwatkunaporn@gmail.com>
2024-08-30 11:41:46 +0700

    lesson guide

--- src/AppTheme.tsx
+++ src/AppTheme.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import React from "react";
+// 1. install "Inter" font from https://fontsource.org/fonts/inter/install and import it
 
 const theme = createTheme({
   palette: {
@@ -7,6 +8,7 @@ const theme = createTheme({
       default: "#f5f5f5",
     },
   },
+  // 2. Add a typography object with a fontFamily property (https://mui.com/material-ui/customization/typography/)
 });
 
 export default function AppTheme({ children }: React.PropsWithChildren) {
