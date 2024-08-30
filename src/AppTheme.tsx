df5c3a27e7660c3ead3ec5f59d23a7edc150319c
siriwatknp <siriwatkunaporn@gmail.com>
2024-08-30 11:43:28 +0700

    lesson 12 solution

--- src/AppTheme.tsx
+++ src/AppTheme.tsx
@@ -1,11 +1,15 @@
-import { ThemeProvider, createTheme } from "@mui/material/styles";
 import React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import "@fontsource-variable/inter";
 
 const theme = createTheme({
   palette: {
     background: {
       default: "#f5f5f5",
     },
+  },
+  typography: {
+    fontFamily: "'Inter Variable', sans-serif",
   },
 });
 
