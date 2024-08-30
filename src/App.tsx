e779c2fb3387e3855355b234084ca73a3b67b6cb
siriwatknp <siriwatkunaporn@gmail.com>
2024-08-30 11:03:41 +0700

    lesson guide

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Button from "@mui/material/Button";
+// 1. import CSS Baseline from Material-UI
 
 function App() {
+  // 2. render CSS Baseline component (it should be at the top of the component tree)
   return <Button>Hello</Button>;
 }
 
