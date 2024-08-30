9a77535a67115796af6b033221fafda120fcef76
siriwatknp <siriwatkunaporn@gmail.com>
2024-08-30 14:30:32 +0700

    lesson 17 solution

--- src/AppTheme.tsx
+++ src/AppTheme.tsx
@@ -7,6 +7,18 @@ const theme = createTheme({
     primary: {
       main: "#27272a",
     },
+    grey: {
+      "50": "#fafafa",
+      "100": "#f5f5f5",
+      "200": "#e5e5e5",
+      "300": "#d4d4d4",
+      "400": "#a3a3a3",
+      "500": "#737373",
+      "600": "#525252",
+      "700": "#404040",
+      "800": "#262626",
+      "900": "#171717",
+    },
     background: {
       default: "#f5f5f5",
     },
@@ -36,6 +48,15 @@ const theme = createTheme({
                 },
                 "&:active": {
                   boxShadow: "none",
+                },
+              },
+            },
+            {
+              props: { variant: "outlined", color: "primary" },
+              style: {
+                borderColor: theme.palette.grey[300],
+                "&:hover": {
+                  borderColor: theme.palette.grey[400],
                 },
               },
             },
