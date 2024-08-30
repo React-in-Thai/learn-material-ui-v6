67222749971b0277dc601a01d8dcf408cc83eb14
siriwatknp <siriwatkunaporn@gmail.com>
2024-08-30 14:25:05 +0700

    lesson 16 solution

--- src/AppTheme.tsx
+++ src/AppTheme.tsx
@@ -17,6 +17,33 @@ const theme = createTheme({
   shape: {
     borderRadius: 8,
   },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          textTransform: "none",
+          variants: [
+            {
+              props: { variant: "contained", color: "primary" },
+              style: {
+                border: `1px solid ${theme.palette.primary.main}`,
+                boxShadow: `inset -0.75px -0.75px 0.75px #000, inset 0.75px 0.75px 0.75px rgba(255, 255, 255, 0.4)`,
+                backgroundImage: `linear-gradient(to bottom, ${theme.palette.primary.light}, ${theme.palette.primary.main})`,
+                backgroundColor: theme.palette.primary.light,
+                "&:hover": {
+                  boxShadow: `inset -0.75px -1px 0.75px #000, inset 0.75px 0 0.75px rgba(255, 255, 255, 0.4)`,
+                  backgroundImage: "none",
+                },
+                "&:active": {
+                  boxShadow: "none",
+                },
+              },
+            },
+          ],
+        }),
+      },
+    },
+  },
 });
 
 export default function AppTheme({ children }: React.PropsWithChildren) {
